8e7f0cc0b002797f6d8733cb5c00f81da0f81aa6
MadDeCoDeR <gkwin8ac@hotmail.gr>
2018-11-26 14:57:57 +0200

    Added Boot sequence and arg parsing

--- src/Menu.java
+++ src/Menu.java
@@ -1,10 +1,51 @@
 import java.util.Scanner;
+import com.JP1.STNFO.*;
+import com.JP1.IO.*;
+import com.JP1.JDBC.*;
+import java.util.*;
 
 public class Menu {
 
     private static Scanner scanner = new Scanner(System.in);
 
+    private static Map<String,Owner> mo;
+    private static Map<String,Insurance> mi;
+    private static FileManager fm;
+    //GK: Totally NOT a rip-off from Classic Doom
+    private static int checkArg(String arg, String [] args){
+        if(args.length > 0){
+            int i=0;
+            for (String s:args){
+                if (s.equals(arg)){
+                    return i;
+                }
+                i++;
+            }
+        }
+        return -1;
+    }
+//GK: Call this in order to retrieve the Data ONCE
+    private static void Boot(String [] args){
+        int file = checkArg("-file",args);
+        if (file >= 0){
+            fm=new FileManager(args[file+1]);
+        }else {
+            fm = new FileManager();
+        }
+        if (fm.HasFile()){
+            mo = fm.getOwnerfromFile();
+            mi = fm.getInsurancefromFile();
+        }else{
+            JDBC db = new JDBC("root","JustPass1");
+            mo = db.RetrieveOwners();
+            mi = db.RetrieveExpDates();
+        }
+
+    }
+
     public static void main(String[] args) {
+
+        Boot(args);
 
         System.out.println("--- Type a number to choose functionality :");
         System.out.println("1. Vehicle Insurance Status");
@@ -20,7 +61,7 @@ public class Menu {
                 System.out.println("Please type your plate(ABC-1234) :");
                 InputManager first = new InputManager();
                 String plate = first.GetPlate();
-                System.out.println(plate);
+                System.out.println(mi.get(plate).getStatus()); //GK: F1 Done???
                 break;
 
             case 2:
